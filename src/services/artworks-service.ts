653cd26b9d15969791e575b907109a95c17fe299
mary <noroozi.m90@gmail.com>
2024-03-13 20:03:38 -0400

    artworkService created

---
+++ src/services/artworks-service.ts
@@ -0,0 +1,19 @@
+import { instance } from "./index";
+
+export const artworkService = {
+  getData: (limit: number, offset: number, page: number) =>
+    instance({
+      method: "GET",
+      url: `/artworks?limit=${limit}&offset=${offset}&page=${page}&fields=id,thumbnail,fiscal_year,dimensions,title,artist_display,date_display,main_reference_number`,
+    }),
+  getById: (id: number | string) =>
+    instance({
+      method: "GET",
+      url: `/artworks/${id}?fields=id,thumbnail,fiscal_year,dimensions,title,artist_display,date_display,main_reference_number`,
+    }),
+  search: (search: string, limit: number, offset: number, page: number) =>
+    instance({
+      method: "GET",
+      url: `/artworks/search?q=${search}&limit=${limit}&offset=${offset}&page=${page}`,
+    }),
+};
